4c2cf9095dd2399a73878fd40fe14c971a81cd77
sagitarisandy <97603275+sagitarisandy@users.noreply.github.com>
2024-01-20 13:31:41 +0700

    Add files via upload
    Express middleware, error handling, Node mongoDB Authentication (login/register), JWT Cookies

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import authRoute from "./routes/auth.js"
 import usersRoute from "./routes/users.js"
 import hotelsRoute from "./routes/hotels.js"
 import roomsRoute from "./routes/rooms.js"
+import cookieParser from "cookie-parser"
 
 const app = express()
 dotenv.config()
@@ -26,13 +27,24 @@ mongoose.connection.on("disconnected", ()=>{
 // })
 
 //middlewares
-
-app.use(express.json())
+app.use(cookieParser());
+app.use(express.json());
 
 app.use("/api/auth", authRoute)
 app.use("/api/users", usersRoute)
 app.use("/api/hotels", hotelsRoute)
 app.use("/api/rooms", roomsRoute)
+
+app.use((err,req,res,next)=>{
+  const errorStatus = err.status || 500;
+  const errorMessaage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessaage,
+    stack: err.stack,
+  })
+})
 
 app.listen(8800, ()=>{
     connect()
