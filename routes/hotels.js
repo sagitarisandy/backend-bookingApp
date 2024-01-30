7a5e02c4f6ea9b05fd7307c98f4e4e81880c0d93
sagitarisandy <97603275+sagitarisandy@users.noreply.github.com>
2024-01-30 09:49:00 +0700

    Add files via upload
    add mongoDB model relations

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
+import { createHotel, deleteHotel, getHotel, getHotels, updateHotel, countByCity } from "../controllers/hotel.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
@@ -11,8 +11,11 @@ router.put("/:id", verifyAdmin, updateHotel);
 //DELETE
 router.delete("/:id", verifyAdmin, deleteHotel)
 //GET
-router.get("/:id", getHotel);
+router.get("/find/:id", getHotel);
 //GET ALL
 router.get("/", getHotels);
+
+router.get("/countByCity", countByCity);
+router.get("/countByType", getHotels);
 
 export default router
